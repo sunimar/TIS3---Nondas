b2e514d9bcd8d42ed422c634192927a978780447
sunimar <43272283+sunimar@users.noreply.github.com>
2020-03-27 16:51:39 -0300

    Entidades novas
    Produto e serviço - entidades codificadas

--- src/Entidades/Produto.java
+++ src/Entidades/Produto.java
@@ -1,5 +1,35 @@
 package Entidades;
 
 public class Produto {
+	private int codProd;
+	private double precoCusto;
+	private double precovenda;
+
+	public Produto() {}
+
+	public int getCodProd() {
+		return codProd;
+	}
+
+	public void setCodProd(int codProd) {
+		this.codProd = codProd;
+	}
+
+	public double getPrecoCusto() {
+		return precoCusto;
+	}
+
+	public void setPrecoCusto(double precoCusto) {
+		this.precoCusto = precoCusto;
+	}
+
+	public double getPrecovenda() {
+		return precovenda;
+	}
+
+	public void setPrecovenda(double precovenda) {
+		this.precovenda = precovenda;
+	}
+
 
 }
--- src/Entidades/Servico.java
+++ src/Entidades/Servico.java
@@ -1,5 +1,36 @@
 package Entidades;
 
 public class Servico {
+	private int codServ;
+	private String nomeServ;
+	private double precoServ;
+	private Produto [] prodRel;
+
+	public Servico() {}
+
+	public int getCodServ() {
+		return codServ;
+	}
+
+	public void setCodServ(int codServ) {
+		this.codServ = codServ;
+	}
+
+	public String getNomeServ() {
+		return nomeServ;
+	}
+
+	public void setNomeServ(String nomeServ) {
+		this.nomeServ = nomeServ;
+	}
+
+	public double getPrecoServ() {
+		return precoServ;
+	}
+
+	public void setPrecoServ(double precoServ) {
+		this.precoServ = precoServ;
+	}
+
 
 }
