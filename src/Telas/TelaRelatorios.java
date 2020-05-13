d0ff0d13b6cbea71bec2b4643ad432877d2a33ed
sunimar <43272283+sunimar@users.noreply.github.com>
2020-05-13 18:43:07 -0300

    Update TelaRelatorios.java
    Agora é possível gerar relatório de faturamento de serviços por mês/ano

--- src/Telas/TelaRelatorios.java
+++ src/Telas/TelaRelatorios.java
@@ -19,21 +19,27 @@ import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
 import DAO.VendasDAO;
+import DAO.ServicoDAO;
 import Entidades.Cliente;
 import Entidades.OrdemVenda;
+import Entidades.OrdemServico;
 import Entidades.Produto;
 
 public class TelaRelatorios {
 	VendasDAO vendasDAO;
 	OrdemVenda ov;
+	ServicoDAO servDAO;
+	OrdemServico os;
 	// JFrame 
 	static JFrame f; 
 
 	// JButton 
-	static JButton btVendas01, btVendas02, btVol; 
+	static JButton btVendas01, btVendas02, btServ01, btVol; 
 	/*
 	 * vendas01= vendas por data
-	 * vendas02= vendas por produto*/
+	 * vendas02= vendas por produto
+	 * serviços01= serviços e total por data
+	 * serviços02= serviços por data e por cliente*/
 	// label to display text 
 	static JLabel l;
 
@@ -42,13 +48,16 @@ public class TelaRelatorios {
 	public TelaRelatorios() throws IOException {
 		ov = new OrdemVenda();
 		vendasDAO = new VendasDAO("VendasDAO");
+		os = new OrdemServico();
+		servDAO = new ServicoDAO("ServicoDAO");
 		// create a new frame with text field and button 
 		f = new JFrame("Relatórios"); 
 		// create a label to display text 
 		l = new JLabel("Selecione a opção desejada:"); 
 		// create a new buttons 
-		btVendas01= new JButton("Vendas totais por data"); 
+		btVendas01= new JButton("Vendas e faturamento por mês/ano"); 
 		btVendas02 = new JButton("Vendas totais por produto");
+		btServ01 = new JButton("Serviços e vendas por mês/ano");
 		btVol = new JButton("Voltar");
 
 		btVendas01.addActionListener(new ActionListener() {
@@ -73,6 +82,20 @@ public class TelaRelatorios {
 			}
 		});
 
+		btServ01.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					serv01();
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				//f.setVisible(false);
+			}
+		});
+
+
 		btVol.addActionListener(new ActionListener() {
 
 			@Override
@@ -90,6 +113,7 @@ public class TelaRelatorios {
 		box.add(l); 
 		box.add(btVendas01); 
 		box.add(btVendas02);
+		box.add(btServ01);
 		box.add(btVol);
 
 		p.add(box);
@@ -102,6 +126,50 @@ public class TelaRelatorios {
 
 		f.show();
 	}//builder
+
+	public void serv01()throws Exception{
+		String data = "";
+		String comp1 = "";
+		float tmp = 0;
+
+		while (data.equals("")) {
+			data = JOptionPane.showInputDialog("Informe a data: mmaaaa");
+		}
+
+		List<OrdemServico> servs = servDAO.getAll();
+		/************************************************************/
+		JPanel panel = new JPanel();
+		panel.setSize(new Dimension(400, 400));
+
+		JTextArea taText = new JTextArea();
+		taText.setEditable(false);
+		JScrollPane spScroll = new JScrollPane(taText);
+		spScroll.setMaximumSize(new Dimension(400, 400));
+		spScroll.setPreferredSize(new Dimension(400, 400));
+		/***************************************************************/
+		taText.setText("");
+
+		for (OrdemServico i : servs) {
+			comp1 = new SimpleDateFormat("MMyyyy").format(i.getData());
+
+			if(data.equals(comp1)) {
+				tmp += i.getValorTotal();
+				taText.append(i + "\n");
+			}else {
+				System.out.println("nao achou!");
+			}
+		}//for
+
+		if(tmp==0) {
+			JOptionPane.showMessageDialog(null, "Não há Serviços nesse período!");
+			f.dispose();
+		}else {
+			taText.append("Total de faturamento de Serviços: " + tmp);
+			panel.add(spScroll);
+			UIManager.put("OptionPane.minimumSize",new Dimension(400, 400));
+			JOptionPane.showMessageDialog(null, panel, "Serviços no período de " + data, JOptionPane.PLAIN_MESSAGE);
+		}
+	}//vendas01
 
 	public void vendas01()throws Exception{
 		String data = "";
@@ -130,19 +198,24 @@ public class TelaRelatorios {
 
 		for (OrdemVenda i : vendas) {
 			comp1 = new SimpleDateFormat("MMyyyy").format(i.getData());
-			
+
 			if(data.equals(comp1)) {
 				tmp += i.getValorTotal();
 				taText.append(i + "\n");
 			}else {
 				System.out.println("nao achou!");
 			}
-		}
-		taText.append("Total: " + tmp);
-		panel.add(spScroll);
-		UIManager.put("OptionPane.minimumSize",new Dimension(400, 400));
-		JOptionPane.showMessageDialog(null, panel, "Vendas", JOptionPane.PLAIN_MESSAGE);
+		}//for
 
+		if(tmp==0) {
+			JOptionPane.showMessageDialog(null, "Não há vendas nesse período!");
+			f.dispose();
+		}else {
+			taText.append("Total de faturamento de vendas: " + tmp);
+			panel.add(spScroll);
+			UIManager.put("OptionPane.minimumSize",new Dimension(400, 400));
+			JOptionPane.showMessageDialog(null, panel, "Vendas no período de " + data, JOptionPane.PLAIN_MESSAGE);
+		}
 	}//vendas01
 
 
